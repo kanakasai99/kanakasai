7a1110d9e3af01043189edb1150490afebda84fa
kanakasai99 <kanakasaimereddy99@gmail.com>
2025-07-09 11:37:26 +0530

    somechanges made in array program

--- src/ArrayPrograms/EvenOddArray.java
+++ src/ArrayPrograms/EvenOddArray.java
@@ -1,22 +1,27 @@
 package ArrayPrograms;
 
+import java.util.ArrayList;
+
 public class EvenOddArray {
 
-	public static void main(String[] args) {
-		
-		int a[]= {1,2,5,6,9,8,3};
-		int even=0;
-		int odd=0;
-		for(int i=0;i<a.length;i++) {
-				if(a[i]%2==0) {
-					even++;
-				}
-				else {
-					odd++;
-				}
-		}
-		System.out.println("count of even numbers:" +even);
-		System.out.println("count of odd numbers:" +odd);
-	}
+    public static void main(String[] args) {
+        
+        int[] a = {1, 2, 5, 6, 9, 8, 3};
 
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                evenNumbers.add(a[i]);
+            } else {
+                oddNumbers.add(a[i]);
+            }
+        }
+
+        System.out.println("Even numbers: " + evenNumbers);
+        System.out.println("Odd numbers: " + oddNumbers);
+        System.out.println("Count of even numbers: " + evenNumbers.size());
+        System.out.println("Count of odd numbers: " + oddNumbers.size());
+    }
 }
