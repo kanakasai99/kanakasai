0af80ec7cdb8bbdfe04eece44972cc854216ac0f
kanakasai99 <kanakasaimereddy99@gmail.com>
2025-09-10 08:43:26 +0530

    updated

--- src/main/java/ArrayPrograms/SumCharacterAndNumbers.java
+++ src/main/java/ArrayPrograms/SumCharacterAndNumbers.java
@@ -2,15 +2,17 @@ package ArrayPrograms;
 public class SumCharacterAndNumbers {
 	
 	    public static void main(String[] args) {
-	      String mylist[]={"2","#@","$12","28","7","**"};
+	      String mylist[]={"2","#@","$11","28","7","**"};
 	      int sum=0;
 	      String Invalid="";
 	      for(String element: mylist){
+			  String digits=element.replaceAll("\\D","");
+
 	          try{
-	          sum=sum+Integer.parseInt(element);
+	          sum=sum+Integer.parseInt(digits);
 	      }
 	      catch(Exception e){
-	    	  Invalid= Invalid +element+", ";
+	    	  Invalid= Invalid +digits+", ";
 	    	 
 	      }
 	    }
