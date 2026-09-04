94bc0d3c09aa07f6a55677f2d69f5d0b10f8812d
kanakasai99 <kanakasaimereddy99@gmail.com>
2026-09-04 13:10:44 +0530

    Good thing

---
+++ src/main/java/stringPrograms/aa2bbb3cccc4d1.java
@@ -0,0 +1,25 @@
+package stringPrograms;
+
+public class aa2bbb3cccc4d1 {
+        public static void main(String[] args) {
+            String s="aabbbcceeee";
+            StringBuilder st=new StringBuilder();
+            int count=1;
+            for(int i=0;i<s.length();i++){
+                if((i+1<s.length()) && (s.charAt(i)==s.charAt(i+1))){
+                    count++;
+                }
+                else{
+                    for(int j=0;j<count;j++){
+                        st.append(s.charAt(i));
+                    }
+                    st.append(count);
+                    count=1;
+                }
+                
+            }
+
+            System.out.println(st);
+        }
+    }
+
